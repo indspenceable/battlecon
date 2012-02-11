a43b725c66eb2fb5d938245de3452d4e7c6445f0
Danny Spencer <daniel.patrick.spencer@gmail.com>
2012-02-11 12:04:42 -0800

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Battlecon::Application.config.secret_token = 'af5eaed711a6d584e4abb5013c0530cbeaa58949811e4ae822c4cf3ae3d71f317567ef859cec6cbfb49941ee94995c7655423d3490d2939ef444246655442887'
