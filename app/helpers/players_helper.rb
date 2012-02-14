679b724a66be7eab434bacf784a837b008824d1b
Danny Spencer <daniel.patrick.spencer@gmail.com>
2012-02-14 09:15:32 -0800

    Display character name, for player wins.

--- app/helpers/players_helper.rb
+++ app/helpers/players_helper.rb
@@ -5,6 +5,6 @@ module PlayersHelper
   def game_link me, g
     p1,p2 = g.plays
     p2,p1 = p1,p2 if me == p2.player 
-    link_to "(#{p1.player.name.titleize}) #{win_loss_phrase(p1.win)} #{p2.character.name.titleize} (#{p2.player.name.titleize})", g     
+    link_to "#{p1.character.name.titleize} #{win_loss_phrase(p1.win)} #{p2.character.name.titleize} (#{p2.player.name.titleize})", g     
   end  
 end
