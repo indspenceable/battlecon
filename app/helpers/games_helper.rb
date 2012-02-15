7e8d1651aeeee062c5754b5674556ed66b181cc8
Danny Spencer <daniel.patrick.spencer@gmail.com>
2012-02-14 21:27:40 -0800

    Use all helpers again, put helper methods in right places.

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,2 +1,15 @@
 module GamesHelper
+  def win_loss_phrase b
+    b ? "WON against" : "LOST to"
+  end
+  def player_game_link me, g
+    p1,p2 = g.plays
+    p2,p1 = p1,p2 if me == p2.player 
+    link_to "#{p1.character.name.titleize} #{win_loss_phrase(p1.win)} #{p2.character.name.titleize} (#{p2.player.name.titleize})", g     
+  end
+  def character_game_link me, g
+    p1,p2 = g.plays
+    p2,p1 = p1,p2 if me == p2.character unless p1.character == p2.character
+    link_to "(#{p1.player.name.titleize}) #{win_loss_phrase(p1.win)} #{p2.character.name.titleize} (#{p2.player.name.titleize})", g     
+  end
 end
--- app/helpers/players_helper.rb
+++ app/helpers/players_helper.rb
@@ -1,10 +1,2 @@
 module PlayersHelper
-  def win_loss_phrase b
-    b ? "WON against" : "LOST to"
-  end
-  def game_link me, g
-    p1,p2 = g.plays
-    p2,p1 = p1,p2 if me == p2.player 
-    link_to "#{p1.character.name.titleize} #{win_loss_phrase(p1.win)} #{p2.character.name.titleize} (#{p2.player.name.titleize})", g     
-  end  
 end
