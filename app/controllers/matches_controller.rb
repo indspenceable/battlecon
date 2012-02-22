80f708ab4afa99a6d691c65769a38e30cb55eeec
Daniel Spencer <daniel.patrick.spencer@gmail.com>
2012-02-22 13:02:37 -0800

    Set the league on new matches fixes #10

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,11 +1,13 @@
 class MatchesController < ApplicationController
   before_filter :require_login!
   def new
-    @match = Match.new(:creator => active_player.id, :creator_name => active_player.name, :league_id => active_league.id)
+    @match = Match.new(:creator => active_player.id, :creator_name => active_player.name)
     @opponents = active_league.players - [active_player]
   end
   def create
-    @match = Match.new(params[:match])
+    @match = Match.new(params[:match]) do |m|
+      m.league = active_league
+    end
     @opponents = active_league.players - [active_player]
     if @match.save
       redirect_to dashboard_path, :flash => {:notice => "Match successfuly recorded."}
