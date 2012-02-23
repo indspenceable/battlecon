3628e6a7c8d65722a5474484d9a2d25dde68ac69
Danny Spencer <daniel.patrick.spencer@gmail.com>
2012-02-22 23:52:10 -0800

    Write strategies on individual characters, fixes #5.

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,6 +1,7 @@
 class CharactersController < ApplicationController  
   def show
     @character = Character.find(params[:id])
+    @post = StrategyPost.new(:primary_character_id => @character.id, :creator_id => active_player.id)
   end
   def versus
     @character = Character.find(params[:id])
