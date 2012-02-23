160c1861312bdafab0e1c54b5f603bd76598502f
Danny Spencer <daniel.patrick.spencer@gmail.com>
2012-02-22 23:42:56 -0800

    create strategy posts on the match up page #5

--- app/controllers/strategy_posts_controller.rb
+++ app/controllers/strategy_posts_controller.rb
@@ -2,4 +2,9 @@ class StrategyPostsController < ApplicationController
   def show
     @post = StrategyPost.find(params[:id])
   end
+  
+  def create
+    @post = StrategyPost.new(params[:strategy_post])
+    redirect_to:back, :flash => @post.save ? {:notice => "Successfully created post."} : {:error => "Unable to create post."}
+  end
 end
