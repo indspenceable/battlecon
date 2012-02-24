ba4ffbb2f860424250cf2b6872ed776712cdef13
Danny Spencer <daniel.patrick.spencer@gmail.com>
2012-02-23 21:19:56 -0800

    Updated colors.

--- app/helpers/league_helper.rb
+++ app/helpers/league_helper.rb
@@ -5,17 +5,30 @@ module LeagueHelper
   end
   
   def wins_color c1,c2
-    return '#FFF' if c1 == c2
+    #return '#FFF' if c1 == c2
     win_list = c1.wins.where(:losing_character_id => c2.id).where(:league_id => active_league.id)
     total = c1.matches_against(c2).where(:league_id => active_league.id)
     wins = (win_list.count + 0.0) / total.count
-    return '#77c' if wins.is_a?(Float) && wins.nan?
+    return 'transparent' if wins.is_a?(Float) && wins.nan?
     
-    r = ((1-wins)*255).to_i.to_s(16)
+    
+    wp = (wins - 0.5) * 2
+    r,g,b = "FF","FF","AA"
+    if wp <= 0
+      g = ((wp+1) * 255).to_i.to_s(16)
+      b = ((wp+1) * 255 * 2 / 3).to_i.to_s(16)
+    else
+      #g = ((wp) * 255).to_i.to_s(16)
+      r = ((1 - wp) * 255).to_i.to_s(16)
+      b = ((1 - wp) * 255 * 2 / 3).to_i.to_s(16)
+    end
+    
+    # r = ((1-wins)*255).to_i.to_s(16)
     r = "0#{r}" if r.length == 1
-    g = (wins*255).to_i.to_s(16)
+    # g = (wins*255).to_i.to_s(16)
     g = "0#{g}" if g.length == 1
-    b = "00"
+    b = "0#{b}" if b.length == 1
+    # b = "00"
     
     return "##{r}#{g}#{b}"
   end
