e6c25c8b3c8c9d66154aa204484c2e33eaae9ea5
Danny Spencer <daniel.patrick.spencer@gmail.com>
2012-02-26 14:07:32 -0800

    Some updates for the guide on the league index.

--- app/helpers/characters_helper.rb
+++ app/helpers/characters_helper.rb
@@ -6,4 +6,7 @@ module CharactersHelper
       return link_to "(#{match.losing_player.name.titleize}) LOST to #{match.winning_character.name.titleize} (#{match.winning_player.name.titleize})"
     end
   end
+  def character_vs_link match
+    return link_to "(#{match.winning_player.name.titleize}) #{match.winning_character.name.titleize} WON against #{match.losing_character.name.titleize} (#{match.losing_player.name.titleize})"
+  end
 end
