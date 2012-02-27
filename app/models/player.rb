f62125429d5344cb7d0108de5b9979154d76da71
Danny Spencer <daniel.patrick.spencer@gmail.com>
2012-02-26 18:17:45 -0800

    Get rid of unused things in player.

--- app/models/player.rb
+++ app/models/player.rb
@@ -21,22 +21,8 @@ class Player < ActiveRecord::Base
   has_many :leagues, :through => :league_memberships
   
   belongs_to :active_league, :class_name => "League"
-  validates :active_league, :presence => true, :unless => :new_league_name
-  after_create do
-    leagues << active_league
-  end
   
   has_many :strategy_posts, :foreign_key => "creator_id"
-  
-  attr_accessor :new_league_name
-  before_create do
-    puts "New league name is #{new_league_name}"
-    if new_league_name && !new_league_name.empty?
-      self.active_league = League.create(:name => new_league_name)
-    else
-      false unless active_league
-    end
-  end
   
   has_secure_password
   validates_presence_of :password, :on => :create
@@ -44,6 +30,5 @@ class Player < ActiveRecord::Base
     
   before_save do
     self.name = self.name.downcase
-    puts "password is ", self.password, " digest is ", self.password_digest
   end
 end
