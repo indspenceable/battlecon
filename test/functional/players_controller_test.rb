c49c3b4716cdf627c04752d80a7bbe5a1416eec1
Danny Spencer <daniel.patrick.spencer@gmail.com>
2012-03-03 15:15:01 -0800

    Removed unit tests

--- test/functional/players_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class PlayersControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/unit/helpers/strategy_posts_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class StrategyPostsHelperTest < ActionView::TestCase
-end
