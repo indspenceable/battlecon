61fd91110e3083f83a163158de1732a58cf7426f
Danny Spencer <daniel.patrick.spencer@gmail.com>
2012-03-04 17:15:47 -0800

    match creation specs

--- spec/requests/create_match_spec.rb
+++ spec/requests/create_match_spec.rb
@@ -11,15 +11,39 @@ describe "creating a match" do
       p.update_attributes(:active_league => league)
     end
   end
-  it "should be able to record a match" do
-    p1,p2 = players
+  let :p1 do
+    players.first
+  end
+  let :p2 do
+    players.last
+  end
+  
+  def record_match as, vs, c1, c2, won
     login! p1
     click_link "Record Play"
-    select 'cherri', :from => 'match_creator_character'
+    select c1, :from => 'match_creator_character'
     select p2.name, :from => 'match_opponent'
-    select 'heketch', :from => 'match_opponent_character'
-    check('match_creator_is_winner')
+    select c2, :from => 'match_opponent_character'
+    if won
+      check('match_creator_is_winner')
+    else
+      check('creator-loser-checkbox')
+    end
     click_on("Create Match")
-    
+  end
+  def check_match m, wp, lp, wc, lc
+    m.winning_character.should == Character.find_by_name(wc)
+    m.losing_character.should == Character.find_by_name(lc)
+    m.winning_player.should == wp
+    m.losing_player.should == lp
+  end
+  
+  it "should be able to record a win" do
+    record_match p1, p2, 'cherri', 'heketch', true
+    check_match ::Match.last, p1, p2, 'cherri', 'heketch'
+  end
+  it "should be able to record a loss" do
+    record_match p1, p2, 'khadath', 'rukyuk', false
+    check_match ::Match.last, p2, p1, 'rukyuk', 'khadath'
   end
 end
